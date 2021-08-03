892b19b1edccbfa1ead3fb3683855a25f8bb910e
csi2 <csi2@grupogema.com.co>
2021-08-03 07:46:44 -0500

    pestaña de inventario en petición de mantenimiento

--- src/addons/maintenance/models/maintenance.py
+++ src/addons/maintenance/models/maintenance.py
@@ -398,11 +398,13 @@ class MaintenanceRequest(models.Model):
     active = fields.Boolean("Active", default="True")
     cierre = fields.Boolean("Active", default="True")
     # gastos_per = fields.Many2one('hr.expense')
-    gastos_personal = fields.One2many('hr.expense', 'id')
-
+    gastos_personal = fields.One2many('hr.expense', 'id', copy=True)
+    inventario_personal = fields.One2many('stock.quant', 'id', copy=True)
     @api.onchange('user_id')
     def default_gastos(self):
-        self.gastos_personal = [(4, 7)]
+        self.gastos_personal = [(6, 0, [2, 3, 4])]
+    def default_inventario(self):
+        self.inventario_personal = [(6, 0, [2, 3, 4])]
 
     @api.model
     def name_search(self, name='', args=None, operator='ilike', limit=100):
